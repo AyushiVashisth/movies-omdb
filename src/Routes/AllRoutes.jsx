67eb0571924997fc610617a4294612b7d2744ced
Ayushi Vashisth <ayushivashisth22@gmail.com>
2024-06-19 02:46:01 +0530

    create router pages

---
+++ src/Routes/AllRoutes.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Navbar from "../components/Navbar";
+
+const AllRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/search/:query" element={<SearchResults />} />
+        <Route path="/movie" element={<Movies />} />
+        <Route path="/series" element={<Series />} /> */}
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
+};
+
+export default AllRoutes;
